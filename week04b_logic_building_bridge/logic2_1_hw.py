992216f14b04c39ca5ce094d927986c51fac96e8
akankshaburle32 <akankshaburle32@gmail.com>
2026-07-24 10:57:58 +0530

    file add

---
+++ week04b_logic_building_bridge/logic2_1_hw.py
@@ -0,0 +1,42 @@
+## Step 1 : REstateEk 
+# que 1.  number lo aur batao woh positive, negative ya zero hai.
+
+## Step 2: ex:  5 = positive, -5 = negative,  0 = zero
+
+## Step 3: Pseudocode
+# 1. ek input no. lo 
+# 2. or vo no. dekho ki vo no. zero se badi hai ya nhi. 
+# 3. badi he vo no. to positive dikhaygi.
+# 4. yadi vo no. zero se chota no. hoga.
+# 5. to vo no. negative dikhaygi.
+# 6. nhi to zero dikhaygi. 
+
+# step 4:
+
+n = int(input ("Entes your any number: "))
+
+if n > 0:
+    print("positive")
+
+elif n < 0:
+    print("Negative")
+
+else:
+    print("zero")
+
+    # step:5
+
+    # step      no.     
+    # S1        5
+    # if        True
+    # print     positive
+
+    # S1        -5
+    # elif      True
+    # print     negative
+
+    # S1        0
+    # else      True
+    # print     zero
+
+        
---
+++ week04b_logic_building_bridge/logic2_2_hw.py
@@ -0,0 +1,33 @@
+## Step 1 : REstateEk 
+##  que 2. Ek list of marks lo aur unka average nikalo.
+
+## step 2: ex.: 10+10+10 = 30 len =3  average = 30/3 =10
+
+## Step 3: pseudocode
+
+# 1. no. ki list lo.
+# 2. total = 0
+# 3. list ke pure no. ko (m) bathaye 
+# 4. total = total + m
+# 5. avrage = total / list me pure no. kitne hai bathaye
+# 6. or uska avrage nikale
+
+## step 4: 
+
+marks = [87, 67, 30, 67, 16]
+
+total = 0
+for m in marks:
+    
+    total = total + m
+average = total / len(m)
+
+print(average)
+
+ # Step 5:
+
+ # step     marks       total
+ # s 1      87            -
+ # f-lp1    87            0
+ # f-lp1    87            87
+ # f-lp2    67
